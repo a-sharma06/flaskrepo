0ffabf9f44555812f900556c6e9f05946d1d1b57
Akshay Sharma <a_sharma06@yahoo.com>
2018-03-25 03:15:22 +0000

    app change

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def about():
     rawData = pd.read_csv(io.StringIO(urlData.decode('utf-8')))
     rawData.Date = pd.to_datetime(rawData.Date)
     # create a new plot with a title and axis labels
-    p = figure(title="Quandl WIKI EOD Stock Price - 2017", x_axis_label='Time', y_axis_label='Price', x_axis_type="datetime")
+    p = figure(tools="pan,wheel_zoom,box_zoom,reset", title="Quandl WIKI EOD Stock Price - 2017", x_axis_label='Time', y_axis_label='Price', x_axis_type="datetime")
 
     # add a line renderer with legend and line thickness
     #plot=p.line(y = rawData.Close, x = rawData.Date, legend = (string + ' - Closing Value'), line_width=2)
