1ec4d1700a5c7070a55c56dba3bf167ed641d840
Akshay Sharma <a_sharma06@yahoo.com>
2018-03-25 02:21:45 +0000

    app change

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ def about():
     ticker = request.form['ticker']
     url = 'https://www.quandl.com/api/v3/datasets/WIKI/FB.csv?column_index=4&start_date=2017-01-01&end_date=2017-12-31&order=asc&ticker='+ticker+'&api_key=Bi6LQVQYYUMzFxnjxMV8'
     urlData = requests.get(url).content
-    #rawData = pd.read_csv(io.StringIO(urlData.decode('utf-8')))
-    #rawData.Date = pd.to_datetime(rawData.Date)
+    rawData = pd.read_csv(io.StringIO(urlData.decode('utf-8')))
+    rawData.Date = pd.to_datetime(rawData.Date)
     # create a new plot with a title and axis labels
     #p = figure(title="Quandl WIKI EOD Stock Price - 2017", x_axis_label='Time', y_axis_label='Price', x_axis_type="datetime")
 
